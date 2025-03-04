f39ec881cfabc4c9fcda99817178ca80577db983
Baskar L <139743997+baskarlmca2023@users.noreply.github.com>
2025-03-04 22:40:47 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://travelplanner-backend-gv0v.onrender.com",
         secure: false,
       },
     },
